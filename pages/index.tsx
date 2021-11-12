5b59dfebcbdc23fd4e17328c9d27bf6c662f5eec
logan <gnlee95@gmail.com>
2021-11-12 16:51:20 +0900

    Update finish practice jwt

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,56 @@
 import { useState } from 'react';
+import jwt from 'jsonwebtoken';
 
 export default function Home() {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
+  const [message, setMessage] = useState<string>('You are not logged in');
+  const [secret, setSecret] = useState<string>('');
+
   async function submitForm() {
     const res = await fetch('/api/login', {
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify({ username, password }),
-    });
+    }).then((t) => t.json());
+
+    const token = res.token;
+
+    if (token) {
+      const json = jwt.decode(token) as { [key: string]: string };
+      console.log(json);
+
+      setMessage(
+        `Welcome ${json.username} and you are ${
+          json.admin ? 'an admin!' : 'not an admin'
+        }`
+      );
+
+      const res = await fetch('/api/secret', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token }),
+      }).then((t) => t.json());
+
+      if (res.secretAdminCode) {
+        setSecret(res.secretAdminCode);
+      } else {
+        setSecret('Nothing available');
+      }
+    } else {
+      setMessage('Something went wrong');
+    }
   }
 
   return (
     <div>
-      <h1>I am index page and not hello</h1>
+      <h1>{message}</h1>
+      <h1>Secret: {secret}</h1>
       <form>
         <input
           type="text"
